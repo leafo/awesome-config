a6b6e9e4a5dd5614510b88d1303e0a2b1841989b
leaf corcoran <leafot@gmail.com>
2013-02-26 13:17:55 -0800

    change default layouts

--- rc.lua
+++ rc.lua
@@ -98,7 +98,7 @@ layouts =
 {
     awful.layout.suit.tile,
     -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
@@ -107,7 +107,7 @@ layouts =
     awful.layout.suit.max,
     -- awful.layout.suit.max.fullscreen,
     awful.layout.suit.floating,
-    awful.layout.suit.magnifier
+    -- awful.layout.suit.magnifier
 }
 -- }}}
 
