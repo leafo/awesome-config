51cd9e8699f167d8bd6f2917f4a195adb8df38e9
leaf corcoran <leafot@gmail.com>
2013-03-19 08:27:33 -0700

    don't run anything it's broken

--- leaf/status.lua
+++ leaf/status.lua
@@ -1,24 +1,41 @@
 local textbox = require("wibox.widget.textbox")
 local awful = require("awful")
+local naughty = require("naughty")
 local make_status
 make_status = function(timeout)
   if timeout == nil then
     timeout = 60
   end
   local w = textbox()
+  local check_running
+  check_running = function()
+    return awful.util.pread("ps x | grep selfspy"):match("python2")
+  end
+  local run
+  run = function()
+    naughty.notify({
+      text = "Starting selfspy..."
+    })
+    return awful.util.spawn_with_shell("selfspy.py -c /home/leafo/.selfspy.conf")
+  end
   local t
   do
     local _with_0 = timer({
       timeout = timeout
     })
     _with_0:connect_signal("timeout", function()
-      local running = awful.util.pread("ps x | grep selfspy | grep python2"):match("%S")
-      return w:set_markup("[selfspy: " .. tostring(running and "ON" or "OFF") .. "] ")
+      return w:set_markup("[selfspy: " .. tostring(check_running() and "ON" or "OFF") .. "] ")
     end)
     _with_0:start()
     _with_0:emit_signal("timeout")
     t = _with_0
   end
+  w:connect_signal("button::press", function()
+    naughty.notify({
+      text = "running: " .. tostring(check_running())
+    })
+    return t:emit_signal("timeout")
+  end)
   return w
 end
 return {
--- leaf/status.moon
+++ leaf/status.moon
@@ -1,16 +1,30 @@
 
 textbox = require "wibox.widget.textbox"
 awful = require "awful"
+naughty = require "naughty"
 
 make_status = (timeout=60) ->
   w = textbox!
+
+  check_running = ->
+    awful.util.pread("ps x | grep selfspy")\match "python2"
+
+  run = ->
+    naughty.notify text: "Starting selfspy..."
+    awful.util.spawn_with_shell "selfspy.py -c /home/leafo/.selfspy.conf"
+
   t = with timer(:timeout)
     \connect_signal "timeout", ->
-      running = awful.util.pread("ps x | grep selfspy | grep python2")\match "%S"
-      w\set_markup "[selfspy: #{running and "ON" or "OFF"}] "
+      w\set_markup "[selfspy: #{check_running! and "ON" or "OFF"}] "
 
     \start!
     \emit_signal "timeout"
+
+  w\connect_signal "button::press", ->
+    -- run! unless check_running!
+    naughty.notify text: "running: #{check_running!}"
+    t\emit_signal "timeout"
+
   w
 
 { :make_status }
